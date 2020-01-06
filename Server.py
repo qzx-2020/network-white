30524032662c33a14ccc68ead23e5ac3298ffe17
Your Name <[Email@example.com](mailto:Email@example.com)>
2020-01-06 14:00:55 +0800

    服务端与客户端链接，心跳线程，用户名称列表

--- Server.py
+++ Server.py
@@ -1,24 +1,79 @@
 import socket
+import threading
 import time
 
 
+
 class Server:
+    Clients = []
     def __init__(self,host,port):
         self.host = host
         self.port = port
 
         self.notwork = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.notwork.bind((self,host,self.port))
+        self.notwork.bind((self.host,self.port))
         self.notwork.listen(20)
 
         print(f'server listen at {self.port}')
+        threading.Thread(target=self.pinger).start()
 
 
+    def pinger(self):
+        while True:
+            time.sleep(0.1)
+            for client in Server.Clients:
+                try:
+                    msg = 'ß'.encode('ISO-8859-1')
+                    print('ß')
+                    client.sock.send(msg)
+                except ConnectionResetError:
+                    print('ConnectionResetError')
+                    client.termainate()
+                    Server.Clients.remove(client)
+                    pass
+                except ConnectionAbortedError:
+                    client.terminate()
+                    Server.Clients.remove(client)
+                    print('ConnectionAbortedError')
+                    pass
     def start(self):
         while True:
             client_sock,client_addr = self.notwork.accept()
             print(f'client {client_addr} connected ')
+
+            client_sock.send('HLO'.encode())
             time.sleep(0.1)
 
+            msg = ' '
+            for client in Server.Clients:
+                msg = msg + ' '+ client.clientID
 
+            client_sock.send(msg.encode('utf-8'))
+
+            client_thread = threading.Thread(target=self.wait_for_user_nickname,args=[client_sock])
+            client_thread.start()
+    def wait_for_user_nickname(self,client_sock):
+        new_user_id = client_sock.recv(1024).decode('utf-8')
+        print(new_user_id)
+        client = Client(client_sock,new_user_id)
+        Server.Clients.append(client)
+        client.start()
+
+class Client:
+    def __init__(self,sock,clentID):
+        self.sock = sock
+        self.clientID =clentID
+        self._run =True
+
+    def terminate(self):
+        self._run = False
+
+    def start(self):
+        while self._run:
+            time.sleep(0.1)
+            pass
+
+if __name__ == '__main__':
+    server = Server('0.0.0.0',6000)
+    server.start()
 
---
+++ UserDialog.py
@@ -0,0 +1,62 @@
+from tkinter import *
+
+
+class UserDialog:
+    _Ip = ''
+    _port = 0
+    _nickname = ''
+    def __init__(self):
+        pass
+    @classmethod
+    def show_error_box(cls,msg):
+        master = Tk()
+        Label(master,text = msg).grid(row =0)
+        Button(master,text ='ok',command =master.destroy()).grid(row =1,pady =4)
+        master.mainloop()
+
+    @classmethod
+    def getUserInputIP(cls):
+        def getUserIPAndPort():
+            cls._Ip = e1.get()
+            cls._port =int(e2.get())
+            ClientWindow.destroy()
+
+        ClientWindow =Tk()
+        Label(ClientWindow,text = '请输入id及port').grid(row = 0)
+        Label(ClientWindow, text='id').grid(row = 1)
+        Label(ClientWindow, text='port').grid(row=2)
+
+        e1 = Entry(ClientWindow)
+        e2 = Entry(ClientWindow)
+
+        e1.grid(row = 1,column =1)
+        e2.grid(row = 2,column =1)
+
+        buttom = Button(ClientWindow)
+        buttom.grid(row =3,column =1)
+        buttom.config(text ='ok',command = getUserIPAndPort)
+
+        ClientWindow.mainloop()
+
+
+    @classmethod
+    def getUserNickName(cls):
+        def getUserNickNameInner():
+            cls._nickname = e1.get()
+
+            ClentWindow.destroy()
+        ClentWindow = Tk()
+        Label(ClentWindow,text ='请输入昵称').grid(row =0)
+        Label(ClentWindow, text='昵称').grid(row=1)
+
+        e1 = Entry(ClentWindow)
+        e1.grid(row =1,column =1)
+
+        buttom = Button(ClentWindow)
+        buttom.grid(row =2,column =1)
+        buttom.config(text ='ok',command =getUserNickNameInner)
+
+        ClentWindow.mainloop()
+
+if __name__ == '__main__':
+    UserDialog.getUserInputIP()
